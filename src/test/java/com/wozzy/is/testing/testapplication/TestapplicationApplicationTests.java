949a15869a8816ab9d4f24dfb0ec3817ab40d103
Wozzy <diepw50@gmail.com>
2024-08-21 16:02:59 -0500

    initial commit

---
+++ src/test/java/com/wozzy/is/testing/testapplication/TestapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wozzy.is.testing.testapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
